55f324d6831bb6ed24f4345155aafa3179add2ae
Enrique Ochoa <eochoa11@ucol.mx>
2021-01-11 02:23:57 -0600

    Actualizacion escena MenuEs
    Se cambia la accion del boton "Jugar" para que redireccione a la pantalla de carga, y ésta a su vez redireccione al nivel

--- Escenas/MenuEs.gd
+++ Escenas/MenuEs.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _on_Jugar_pressed():
 # warning-ignore:return_value_discarded
-	get_tree().change_scene("res://Escenas/Nivel1.tscn")
+	get_tree().change_scene("res://Loadlvl.tscn")
 
 func _on_Salir_pressed():
 	get_tree().quit()
