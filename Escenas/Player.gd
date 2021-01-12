9b4d23a2440b880555a4239fa4cf40d2b4eaed3c
Enrique Ochoa <eochoa11@ucol.mx>
2021-01-12 16:01:24 -0600

    Actualizacion escena Player
    Se añade el efecto de salto al jugador

--- Escenas/Player.gd
+++ Escenas/Player.gd
@@ -4,12 +4,14 @@ var speed = 200 #Variable speed añadida, pixeles por segundo
 var velocity = Vector2 (0, 0) #Velocidad lineal y vertical
 var gravity = 750 #Variable de gravedad
 var jump = -350 #Variable de salto con NEGATIVO porque el eje  Y superior se indica en negativo, y en positivo para invertirlo
+onready var sound = get_node("Jump")
 
 onready var sprite = $Sprite
 onready var animation = $AnimationPlayer
 
 func _ready():
 	OS.center_window()
+
 func _physics_process(delta): #Siempre que se trabajen con físicas se usa ésta función
 	velocity.y += gravity * delta #Gravedad activa
 	velocity.x = 0
@@ -27,7 +29,10 @@ func _physics_process(delta): #Siempre que se trabajen con físicas se usa ésta
 		animation.play("Idle")
 		friction = true
 	
+	
 	if is_on_floor() and Input.is_action_pressed("ui_up"):
+		sound.stream.loop = false
+		sound.play()
 		velocity.y = jump #Funcion de salto
 		if friction == true:
 			velocity.x = lerp(velocity.x, 0, 0.5)
