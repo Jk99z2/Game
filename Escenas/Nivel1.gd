231ff9e4d0a66f29299bded4a64dcb83f3f0f4ee
Enrique Ochoa <eochoa11@ucol.mx>
2021-01-12 16:02:14 -0600

    Escena NIvel 1 completada

--- Escenas/Nivel1.gd
+++ Escenas/Nivel1.gd
@@ -2,21 +2,31 @@ extends Node
 
 func _ready():
 	gamehandler.update_puntos()
+	gamehandler.update_time()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	
+func _physics_process(delta):
+	if Input.is_action_just_pressed("Escape"):
+		if get_tree().paused == true:
+			get_tree().paused = false
+			get_node("Timer").paused = false
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+			get_node("PauseEs").visible = false
+		else:
+			get_tree().paused = true
+			get_node("Timer").paused = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+
 func _on_Timer_timeout():
 	if(gamehandler.tiempo == 120):
 		get_node("AudioStreamPlayer").play()
-	elif(gamehandler.tiempo == 40):
+	if(gamehandler.tiempo == 30):
 		get_node("AudioStreamPlayer").queue_free()
 		get_node("Fast").play()
-	elif(gamehandler.tiempo == 0):
-		get_tree().paused = true
-
 	if(gamehandler.tiempo > 0):
 		gamehandler.tiempo -= 1
 		gamehandler.update_time()
-
-
-
-func _on_AudioStreamPlayer_finished():
-	pass
+	else:
+		get_tree().change_scene("res://Escenas/GameOverEs.tscn")
