10ce8673eb1f3251f9f82451d43303045afb2645
Enrique Ochoa <eochoa11@ucol.mx>
2021-01-12 15:55:14 -0600

    Creditos Eng Script

---
+++ Creditos/CreditEs.gd
@@ -0,0 +1,12 @@
+extends VideoPlayer
+
+# warning-ignore:unused_argument
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_accept"):
+# warning-ignore:return_value_discarded
+		get_tree().change_scene("res://Escenas/MenuEs.tscn")
+
+
+func _on_VideoPlayer_finished():
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Escenas/MenuEs.tscn")
