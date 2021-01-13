46be87e78cff3c36571f293bb6105c1a084d25c1
Enrique Ochoa <eochoa11@ucol.mx>
2021-01-13 00:31:08 -0600

    Creacion de nivel en inglés
    S
    e crea la escena a la que es direccionada el menu play en ingles

---
+++ Nivel1Eng.gd
@@ -0,0 +1,46 @@
+extends Node
+
+func _ready():
+	gamehandler.update_puntos()
+	gamehandler.update_time()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+func _physics_process(delta):
+	if Input.is_action_just_pressed("Escape"):
+		if get_tree().paused == true:
+			get_tree().paused = false
+			get_node("Timer").paused = false
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+			get_node("Reanudar").visible = false
+			get_node("Reanudar2").visible = false
+			get_node("Reanudar3").visible = false
+
+		else:
+			get_tree().paused = true
+			get_node("Timer").paused = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			get_node("Reanudar").visible = true
+			get_node("Reanudar2").visible = true
+			get_node("Reanudar3").visible = true
+
+
+func _on_Timer_timeout():
+	if(gamehandler.tiempo == 120):
+		get_node("AudioStreamPlayer").play()
+	if(gamehandler.tiempo == 30):
+		get_node("AudioStreamPlayer").queue_free()
+		get_node("Fast").play()
+	if(gamehandler.tiempo > 0):
+		gamehandler.tiempo -= 1
+		gamehandler.update_time()
+	if(gamehandler.tiempo == 0):
+		get_tree().change_scene("res://OverEng.tscn")
+
+
+func _on_Reanudar_pressed():
+			get_tree().paused = false
+			get_node("Timer").paused = false
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+			get_node("Reanudar").visible = false
+			get_node("Reanudar2").visible = false
+			get_node("Reanudar3").visible = false
